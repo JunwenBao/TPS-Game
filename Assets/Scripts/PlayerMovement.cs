8789ae8981ec4629df86dec6273166d1b226daf2
Wayents <JunwenBao@users.noreply.github.com>
2025-06-29 12:47:31 +0800

    Seperated PlayerAim from Player Movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -12,19 +12,14 @@ public class PlayerMovement : MonoBehaviour
     [Header("Movement Info")]
     [SerializeField] private float walkSpeed;
     [SerializeField] private float runSpeed;
+
     private float speed;
-    private Vector3 movementDirection;
     private float verticalVelocity;
-    private bool isRunning;
 
-    [Header("Aim Info")]
-    [SerializeField] private Transform aim;
-    [SerializeField] private LayerMask aimLayerMask;
-    private Vector3 lookingDirection;
-
-
+    private Vector3 movementDirection;
     private Vector2 moveInput;
-    private Vector2 aimInput;
+
+    private bool isRunning;
 
     private void Start()
     {
@@ -41,7 +36,7 @@ public class PlayerMovement : MonoBehaviour
     private void Update()
     {
         ApplyMovement();
-        AimTowardMouse();
+        ApplyRotation();
         AnimatorControllers();
     }
 
@@ -59,20 +54,13 @@ public class PlayerMovement : MonoBehaviour
     }
 
     // 控制角色朝向鼠标方向
-    private void AimTowardMouse()
+    private void ApplyRotation()
     {
-        Ray ray = Camera.main.ScreenPointToRay(aimInput);
+        Vector3 lookingDirection = player.aim.GetMousePosition() - transform.position;
+        lookingDirection.y = 0f;
+        lookingDirection.Normalize(); // 获取一个没有长度的向量
 
-        if (Physics.Raycast(ray, out var hitInfo, Mathf.Infinity, aimLayerMask))
-        {
-            lookingDirection = hitInfo.point - transform.position;
-            lookingDirection.y = 0f;
-            lookingDirection.Normalize(); // 获取一个没有长度的向量
-
-            transform.forward = lookingDirection;
-
-            aim.position = new Vector3(hitInfo.point.x, transform.position.y + 1, hitInfo.point.z); 
-        }
+        transform.forward = lookingDirection;
     }
 
     // 控制角色移动
@@ -108,9 +96,6 @@ public class PlayerMovement : MonoBehaviour
 
         controls.Character.Movement.performed += context => moveInput = context.ReadValue<Vector2>();
         controls.Character.Movement.canceled += context => moveInput = Vector2.zero;
-
-        controls.Character.Aim.performed += context => aimInput = context.ReadValue<Vector2>();
-        controls.Character.Aim.canceled += context => aimInput = Vector2.zero;
 
         controls.Character.Run.performed += context =>
         {
